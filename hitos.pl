3076969d29186d2835128cffe25cb6e69ad5bf7e
JJ Merelo <jjmerelo@gmail.com>
2018-01-16 08:55:36 +0100

    Working thanks to @szabgab or something

--- hitos.pl
+++ hitos.pl
@@ -6,16 +6,6 @@ use Dancer2;
 use File::Slurper qw(read_text);
 
 set content_type => 'application/json';
-set port => 31415;
-set engines => {
-		logger => {
-			   File => {
-				    log_dir   => "/tmp",
-				    file_name => 'p5hitos.log',
-				   }
-			  }
-	       };
-set logger => 'File';
 
 my $path;
 for my $p ( qw( hitos.json /data/hitos.json ./data/hitos.json ../data/hitos.json) ) {
